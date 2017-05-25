a7cf4426452b54dd834d00ff5aa8cf34815979c7
ykxie <ykxie@kaikeba.com>
2017-05-25 19:14:04 +0800

    add code demo

---
+++ src/components/code.vue
@@ -0,0 +1,37 @@
+<template>
+  <textarea></textarea>
+</template>
+
+<script>
+  /* eslint-disable */
+  import CodeMirror from 'codemirror'
+  import 'codemirror/lib/codemirror.css'
+  import 'codemirror/mode/javascript/javascript'
+  import 'codemirror/theme/base16-dark.css'
+  export default {
+    data() {
+      return {
+        options: {
+          value: "Hello world",
+          mode: 'text/javascript',
+          lineNumbers: true,
+          smartIndent: false,
+          theme: 'base16-dark'
+        }
+      }
+    },
+    created() {
+    },
+    mounted: function() {
+      console.log(this.$el)
+      this.editor = CodeMirror.fromTextArea(this.$el, this.options)
+      // this.editor.setValue('Hello world');
+      CodeMirror.on(this.editor, "change", function(instance, changeObj) {
+        // console.log(instance);
+        console.log(changeObj);
+      });
+    },
+    methods: {
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import code from '@/components/code'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/code',
+      name: 'code',
+      component: code
     }
   ]
 })
